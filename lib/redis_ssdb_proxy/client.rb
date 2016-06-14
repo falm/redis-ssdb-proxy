79da51be04126d71ae1a345ef30852042f692f87
Jason Hou <hjj1992@gmail.com>
2016-06-14 14:46:54 +0800

    entrie read behavior send to master

--- lib/redis_ssdb_proxy/client.rb
+++ lib/redis_ssdb_proxy/client.rb
@@ -1,9 +1,9 @@
 
 module RedisSsdbProxy
 
-  attr_accessor :master, :slave
-
   class Client
+
+    attr_accessor :master, :slave
 
     def initialize(args)
       sellf.master, self.slave = args.fetch(:master), args.fetch(:slave)
@@ -45,6 +45,46 @@ module RedisSsdbProxy
       end
 
     end
+
+
+    send_to_master :dbsize
+    send_to_master :exists
+    send_to_master :get
+    send_to_master :getbit
+    send_to_master :getrange
+    send_to_master :hexists
+    send_to_master :hget
+    send_to_master :hgetall
+    send_to_master :hkeys
+    send_to_master :hlen
+    send_to_master :hmget
+    send_to_master :hvals
+    send_to_master :keys
+    send_to_master :lindex
+    send_to_master :llen
+    send_to_master :lrange
+    send_to_master :mget
+    send_to_master :randomkey
+    send_to_master :scard
+    send_to_master :sdiff
+    send_to_master :sinter
+    send_to_master :sismember
+    send_to_master :smembers
+    send_to_master :sort
+    send_to_master :srandmember
+    send_to_master :strlen
+    send_to_master :sunion
+    send_to_master :ttl
+    send_to_master :type
+    send_to_master :zcard
+    send_to_master :zcount
+    send_to_master :zrange
+    send_to_master :zrangebyscore
+    send_to_master :zrank
+    send_to_master :zrevrange
+    send_to_master :zscore
+  
+
 
   end # Client
 
