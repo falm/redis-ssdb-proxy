f4ba7359b6e3333c87a106d856c140ea4c4b03fe
Jason Hou <hjj1992@gmail.com>
2016-06-14 11:45:10 +0800

    add delegate method

---
+++ lib/redis_ssdb_proxy/client.rb
@@ -0,0 +1,45 @@
+
+module RedisSsdbProxy
+
+  attr_accessor :master, :slave
+
+  class Client
+
+    def initialize(*args)
+
+    end
+
+    def delegate_ssdb_unsupport(redis_client)
+      redis_client.instance_eval do
+        def sadd(key, *args)
+          zadd(key, args.map{ |arg|
+            [Time.now.to_i, arg]
+          })
+        end
+
+        def srem(*args)
+          zrem(*args)
+        end
+      end
+    end
+
+
+    def send_to_slave(command)
+      class_eval <<-EOS
+        def #{command}(*args, &block)
+          slave.#{command}(*args, &block)
+        end
+      EOS
+    end
+
+    def send_to_master(command)
+      class_eval <<-EOS
+        def #{command}(*args, &block)
+          master.#{command}(*args, &block)
+        end
+      EOS
+    end
+
+  end
+
+end
