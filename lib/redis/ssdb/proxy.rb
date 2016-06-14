d6205bced077ae3e561bc54f4c76742c86c0b96d
Jason Hou <hjj1992@gmail.com>
2016-06-14 09:36:57 +0800

    first commit

---
+++ lib/redis/ssdb/proxy.rb
@@ -0,0 +1,9 @@
+require "redis/ssdb/proxy/version"
+
+module Redis
+  module Ssdb
+    module Proxy
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/redis/ssdb/proxy/version.rb
@@ -0,0 +1,7 @@
+module Redis
+  module Ssdb
+    module Proxy
+      VERSION = "0.1.0"
+    end
+  end
+end
