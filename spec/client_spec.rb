cd6cbfa20cf517d574e540f0b506f6818adb77dc
Jason Hou <hjj1992@gmail.com>
2016-06-17 09:17:23 +0800

    add test case write to both and delegate set to zset

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -12,13 +12,27 @@ describe RedisSsdbProxy do
   end
 
   it 'should works' do
-    @redis.set(:name, 'Tom')
+    redis_to_ssdb_proxy.set(:name, 'Tom')
     expect(redis_to_ssdb_proxy.get(:name)).to eq(@redis.get(:name))
   end
 
   it 'should read from master' do
     expect(redis_to_ssdb_proxy.master).to receive(:get)
     redis_to_ssdb_proxy.get(:lines)
+  end
+
+  it 'should write to redis and ssdb' do
+    args = [:quotes, 'starwars']
+    redis_to_ssdb_proxy.hset( *(args + ['May the force be with you']) )
+    expect(@redis.hget(*args)).to eq(@ssdb.hget(*args))
+  end
+
+  it 'delagate set to zset' do
+    quotes = ["I'm your father", "Thats's no moon", 'Use the force, Luke']
+    key = "#{rand(20)}_quotes_of_star_wars"
+    quotes.each { |q| redis_to_ssdb_proxy.sadd(key, q) }
+    result = @ssdb.zrange(key, 0, -1)
+    expect(result).to eq(quotes)
   end
 
 
