e866ce3bf853afe2228fc1626be5a3fc51569065
Jason Hou <hjj1992@gmail.com>
2016-08-22 16:01:03 +0800

    refactor

--- lib/redis_ssdb_proxy/client.rb
+++ lib/redis_ssdb_proxy/client.rb
@@ -38,43 +38,11 @@ module RedisSsdbProxy
       end
     end
 
-
-    send_to_master :dbsize
-    send_to_master :exists
-    send_to_master :get
-    send_to_master :getbit
-    send_to_master :getrange
-    send_to_master :hexists
-    send_to_master :hget
-    send_to_master :hgetall
-    send_to_master :hkeys
-    send_to_master :hlen
-    send_to_master :hmget
-    send_to_master :hvals
-    send_to_master :keys
-    send_to_master :lindex
-    send_to_master :llen
-    send_to_master :lrange
-    send_to_master :mget
-    send_to_master :randomkey
-    send_to_master :scard
-    send_to_master :sdiff
-    send_to_master :sinter
-    send_to_master :sismember
-    send_to_master :smembers
-    send_to_master :sort
-    send_to_master :srandmember
-    send_to_master :strlen
-    send_to_master :sunion
-    send_to_master :ttl
-    send_to_master :type
-    send_to_master :zcard
-    send_to_master :zcount
-    send_to_master :zrange
-    send_to_master :zrangebyscore
-    send_to_master :zrank
-    send_to_master :zrevrange
-    send_to_master :zscore
+    %i(dbsize exists get getbit getrange hexists hget hgetall hkeys hlen hmget hvals keys lindex llen lrange mget
+    randomkey scard sdiff sinter sismember smembers sort srandmember strlen sunion ttl type zcard zcount zrange
+    zrangebyscore zrank zrevrange zscore).each do |command|
+      send_to_master command
+    end
 
     # all write opreate send to master slave both
     def method_missing(name, *args, &block)
